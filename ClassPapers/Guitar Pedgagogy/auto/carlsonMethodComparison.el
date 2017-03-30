baaee7e1110a6b91bed7d8f9d03e34bcefd7445e
rachaelcarlson <info@rachaelcarlson.com>
2017-03-29 21:06:49 -0500

    Initial commit.

---
+++ ClassPapers/Guitar Pedgagogy/auto/carlsonMethodComparison.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "carlsonMethodComparison"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=1in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "fontspec"
+    "geometry"))
+ :latex)
+
