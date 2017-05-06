e0af7f70fd9d0b01568757cfa9ae43c663ecae05
Rachael Carlson <info@rachaelcarlson.com>
2017-05-05 21:36:55 -0500

    finished reading response 11

---
+++ ClassPapers/Music 314/Reading Response 11/auto/carlsonReadingResponse.el
@@ -0,0 +1,26 @@
+(TeX-add-style-hook
+ "carlsonReadingResponse"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=1in")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "geometry"
+    "fontspec"
+    "hyperref"
+    "biblatex-chicago")
+   (LaTeX-add-bibliographies
+    "bibliography"))
+ :latex)
+
