8ceb22db805ff73171fc2bfc41016907c6be4574
rachaelcarlson-mac <info@rachaelcarlson.com>
2017-05-19 12:37:53 -0500

    Finished transcribing interviews

---
+++ ClassPapers/Guitar Pedgagogy/Interviews/Millham/auto/millhamInterview.el
@@ -0,0 +1,26 @@
+(TeX-add-style-hook
+ "millhamInterview"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ulem" "normalem")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "fontspec"
+    "graphicx"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "hyperref"))
+ :latex)
+
---
+++ ClassPapers/Guitar Pedgagogy/Interviews/auto/MichaelChapdelaineInterview.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "MichaelChapdelaineInterview"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "fontspec"
+    "graphicx"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "hyperref"))
+ :latex)
+
